2458e5eb8489595cab55a1960eacf455296f2773
Ivan Prasmytski <ivan_prasmytski@epam.com>
2018-04-03 17:23:53 +0300

    add react-hot-loader

--- src/games/math/addition/addition.js
+++ src/games/math/addition/addition.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
+import { hot } from 'react-hot-loader'
 import style from './addition.css';
 
 export class Addition extends Component {
   render() {
     return (
       <div className={style.addition}>
-        <h1>Addition</h1>
+        <h2>Addition game</h2>
       </div>
     );
   }
 }
 
-export default Addition;
+export default hot(module)(Addition);
