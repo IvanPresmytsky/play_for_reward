33c4111a8a5eacba29b6bec56f74482dcf895b21
Ivan Prasmytski <ivan.presmytsky@gmail.com>
2018-12-17 21:24:32 +0300

    add validation for nameInput

--- src/_common/components/input/inputFactory.js
+++ src/_common/components/input/inputFactory.js
@@ -5,38 +5,46 @@ import React, { Fragment } from 'react';
 import styles from './input.css';
 
 const Input = ({
+  errorText,
   id,
   labelText,
   onChange,
   type,
+  value,
 }) => {
   const inputClasses = classNames(styles.input);
-  const islabelShown = id && labelText;
+  const islabelShown = id && (labelText || errorText);
+  const labelContent = errorText || labelText;
 
   return (
     <Fragment>
       {islabelShown && (
-        <label htmlFor={id}>{labelText}</label>
+        <label htmlFor={id}>{labelContent}</label>
       )}
       <input
         className={inputClasses}
         id={id}
         type={type}
         onChange={onChange}
+        value={value}
       />
     </Fragment>
   );
 };
 
 Input.defaultProps = {
+  errorText: null,
   id: null,
   labelText: null,
+  value: '',
 };
 
 Input.propTypes = {
+  errorText: PropTypes.string,
   id: PropTypes.string,
   labelText: PropTypes.string,
   type: PropTypes.string.isRequired,
+  value: PropTypes.string,
 };
 
 export default Input;
--- src/_common/components/input/nameInput/nameInput.js
+++ src/_common/components/input/nameInput/nameInput.js
@@ -2,20 +2,39 @@ import { Input } from '~/_common/components/input';
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
-const validateInput = (value) => value.test(/[a-z, A-Z, 0-9]/i) && value;
+const validateSymbol = symbol => (/[a-z0-9_()@!-]/i).test(symbol);
 
 const NameInput = ({ id, labelText }) => {
   const [value, setValue] = useState('');
+  const [error, setError] = useState(null);
 
   const onChangeValue = e => {
-    debugger;
-    if(!(e && e.target)) return false;
-    const x = e.target.value;
-    setValue(x);
+    if (!e) return false;
+
+    e.preventDefault();
+    const nextValue = e.target && e.target.value;
+
+    if (nextValue.length < value.length) {
+      setValue(nextValue);
+      setError(null);
+      return false;
+    }
+
+    const currentSymbol = nextValue && nextValue[nextValue.length - 1];
+    const isValidSymbol = currentSymbol && validateSymbol(currentSymbol);
+
+    if (isValidSymbol) {
+      setValue(nextValue);
+      setError(null);
+    } else {
+      setValue(value);
+      setError(`"${currentSymbol}" is not allowed for username`);
+    }
   };
 
   return (
     <Input
+      errorText={error}
       id={id}
       labelText={labelText}
       type="name"
